487a58d2aa9e3a482936282c5a176fc6a54e5738
OlenaTaras <elena_taras@ukr.net>
2019-04-10 23:37:38 +0300

    merged with master
    added UI login form

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { StoreComponent } from './store.component';
 
 @NgModule({
-  declarations: [StoreComponent],
-  imports: [
-    CommonModule
-  ]
+  declarations: [],
+  imports: [CommonModule],
+  exports: [],
 })
-export class StoreModule { }
+export class StoreModule {}

2e9803d31aa262cec220402181ea9eae53642e49
Mykola Slepko <slepko13@gmail.com>
2019-04-16 00:46:54 +0300

    added footer's elements

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MenuItem} from 'primeng/api';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
@@ -7,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   year: number = new Date().getFullYear();
+  items: MenuItem [];
+  infos: MenuItem [];
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.items = [
+      {
+          label: 'Contacts',
+          items: [
+              {label: 'email: eStoreProject@gmail.com'},
+              {label: 'tel.number: +380342255555'},
+              {label: 'facebook: www.facebook....'}
+          ]
+      },
+
+  ];
+    this.infos = [
+    {
+          label: 'About us',
+          items: [
+            {label: 'Some info about team'}
+
+        ]
+    },
+
+];
+  }
 }

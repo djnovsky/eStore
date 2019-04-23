715da9a67d6b95344ac597ad4fe62d95952dce58
OlenaTaras <elena_taras@ukr.net>
2019-04-23 22:41:16 +0300

    basket is opened in p-slider

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,18 +4,23 @@ import { StoreComponent } from './store.component';
 import { CardModule } from 'primeng/card';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
-import {PanelModule, SidebarModule, SplitButtonModule} from 'primeng/primeng';
+import { PanelModule, SidebarModule, SplitButtonModule } from 'primeng/primeng';
 import { MenubarModule } from 'primeng/menubar';
 import { FormsModule } from '@angular/forms';
 import { DialogModule } from 'primeng/dialog';
 import { PaginatorModule } from 'primeng/paginator';
 import { RadioButtonModule } from 'primeng/radiobutton';
 import { BasketComponent } from './header/basket/basket.component';
-import {TableModule} from 'primeng/table';
-import {DataViewModule} from 'primeng/dataview';
+import { TableModule } from 'primeng/table';
+import { DataViewModule } from 'primeng/dataview';
 
 @NgModule({
-  declarations: [StoreComponent, HeaderComponent, CardComponent, BasketComponent],
+  declarations: [
+    StoreComponent,
+    HeaderComponent,
+    CardComponent,
+    BasketComponent,
+  ],
   imports: [
     CommonModule,
     CardModule,
